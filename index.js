b175e40e91dbf63317c1ef88cc14c581903a72ab
Masaaki Morishita <ikasama3.0@gmail.com>
2015-03-10 21:31:50 +0900

    first implementation

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-module.exports = 
+var annotation = require('css-annotation')
+var postcss = require('postcss')
+var inspect = require('obj-inspector')
+
+module.exports = function (css) {
+    var root = postcss.parse(css)
+
+    var flags = []
+    var tmpNodes = []
+    var results = []
+    root.each(function (node) {
+        if (node.type === 'comment') {
+            var res = annotation.read(node.text)
+            if (res.start) {
+                flags.push({
+                    name: res.start,
+                    start: true,
+                    end: false
+                })
+                return
+            }
+            if (res.end){
+                flags.forEach(function (flag) {
+                    if (flag.name === res.end) {
+                        flag.end = true
+                    }
+                })
+            }
+        }
+
+        flags.forEach(function (flag) {
+            if (flag.start === true && flag.end === false && node.type !== "comment") {
+                tmpNodes.push(node)
+            }
+
+            if (flag.start === true && flag.end === true) {
+                results.push({
+                    name: flag.name,
+                    nodes: tmpNodes
+                })
+                tmpNodes = []
+            }
+        })
+
+    })
+
+    return results
+}
--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 var cssAnnotationBlock = require('./')
+var test = require('tape')
+var fs = require('fs')
+
+test('test', function (t) {
+    var fixture = fs.readFileSync('fixture.css', 'utf-8').trim()
+    cssAnnotationBlock(fixture)
+})
