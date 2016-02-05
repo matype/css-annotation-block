4daa567eaa54faa3b516acf6f70a80cbfc72f4e4
morishitter <morishitter@gmail.com>
2016-02-05 19:36:31 +0900

    update dependencies and write test code

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
-var cssAnnotationBlock = require('./')
-var test = require('tape')
 var fs = require('fs')
+var test = require('tape')
+var postcss = require('postcss')
+var cssAnnotationBlock = require('./')
 
 test('test', function (t) {
     var fixture = fs.readFileSync('fixture.css', 'utf-8').trim()
-    cssAnnotationBlock(fixture)
+    var actual = cssAnnotationBlock(fixture)
+
+    t.equal(actual[0].name, 'color')
+    t.equal(actual[0].nodes.length, 1)
+    t.end()
 })
